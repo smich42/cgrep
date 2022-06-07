10b0d6f6ab24d86120e9af4344fdac155c240912
Stavros Michalovits <stmichalovits@gmail.com>
2022-06-07 15:51:19 +0300

    Document matcher

--- search/matching.go
+++ search/matching.go
@@ -13,11 +13,13 @@ func Match(needle, haystack string, threshold float32) *[]string {
 	needle = clean(needle)
 
 	words := strings.Split(haystack, " ")
+	// All matches must have the same word count as the search pattern.
 	wordCount := len(strings.Split(needle, " "))
 
 	matches := []string{}
 
 	for i := 0; i < len(words)-wordCount; i++ {
+		// Produce the next sentence with the same word count as the search pattern.
 		candidate := words[i]
 		for j := 1; j < wordCount; j++ {
 			candidate += " " + words[i+j]
@@ -31,11 +33,15 @@ func Match(needle, haystack string, threshold float32) *[]string {
 	return &matches
 }
 
+// Ranks the similarity of two strings from 0 to 1.
 func similarity(a, b string) float32 {
 	return indexset.Jaccard(bigramsOf(a), bigramsOf(b))
 }
 
+// Produces an IndexSet containing all bigrams in the given string.
 func bigramsOf(s string) *indexset.IndexSet[bigram] {
+	// Bigrams range from '  ' (index 0) to 'zz' (index 26*26).
+	// The IndexSet must accommodate them all.
 	possibleBigrams := int(LastBigram().Idx())
 	bigrams := indexset.NewIndexSet[bigram](possibleBigrams)
 
