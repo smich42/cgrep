f7b278a39403aa4835ee26c4e9a18eb50de6cdb9
Stavros Michalovits <stmichalovits@gmail.com>
2022-06-09 16:29:44 +0300

    Parse command line arguments

--- main.go
+++ main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/smich42/cgrep/search"
 )
 
+const usageHelp = "Usage: cgrep '[search string]' [0-1.0 similarity] [directory]"
+
 func main() {
-	results, _ := search.SearchDir("quick brown fox", "testdata/", 0.5)
+	argcnt := len(os.Args) - 1
+
+	// Require at least two arguments: pattern and similarity threshold.
+	if argcnt <= 1 {
+		fmt.Println(usageHelp)
+		return
+	}
+
+	searchPattern := os.Args[1]
+	searchThreshold, converr := strconv.ParseFloat(os.Args[2], 32)
+	// Ensure threshold could be parsed.
+	if converr != nil {
+		fmt.Println("Second argument must be a floating-point number.")
+		return
+	}
+	// Default to current working directory if not provided.
+	searchPath := "."
+	if argcnt >= 3 {
+		searchPath = os.Args[3]
+	}
+
+	results, _ := search.SearchDir(searchPattern, searchPath, float32(searchThreshold))
+
 	for filepath, matches := range *results {
-		fmt.Println(filepath)
 		for _, match := range *matches {
-			fmt.Println("\t" + match)
+			fmt.Printf("[%s] '%s'\n", filepath, match)
 		}
 	}
 }
